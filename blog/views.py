0155a769638f1f64685c47b9299fd08aa6143097
beltranbot <beltranbot@gmail.com>
2022-05-02 20:17:37 -0500

    finished stored posts page

--- blog/views.py
+++ blog/views.py
@@ -28,6 +28,13 @@ class AllPostsView(ListView):
 
 
 class SinglePostView(View):
+    def is_stored_post(self, request, post_id):
+        stored_posts = request.session.get("stored_posts")
+        if stored_posts is not None:
+            is_saved_for_later = post_id in stored_posts
+        else:
+            is_saved_for_later = False
+        return is_saved_for_later
 
     def get(self, request, slug):
         post = Post.objects.get(slug=slug)
@@ -35,7 +42,8 @@ class SinglePostView(View):
             "post": post,
             "post_tags": post.tags.all(),
             "comment_form": CommentForm(),
-            "comments": post.comments.all().order_by("-id")
+            "comments": post.comments.all().order_by("-id"),
+            "saved_for_later": self.is_stored_post(request, post.id)
         }
         return render(
             request,
@@ -87,5 +95,7 @@ class ReadLaterView(View):
         post_id = int(request.POST["post_id"])
         if post_id not in stored_posts:
             stored_posts.append(post_id)
-            request.session["stored_posts"] = stored_posts
+        else:
+            stored_posts.remove(post_id)
+        request.session["stored_posts"] = stored_posts
         return HttpResponseRedirect("/")
